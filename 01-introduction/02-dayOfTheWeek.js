33fa8963c47a993e89d807982414b19ced330432
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2025-05-16 18:30:06 +0300

    second task done

---
+++ 01-introduction/02-dayOfTheWeek.js
@@ -0,0 +1,12 @@
+function dayOfWeek(dayNum) {
+    var Days;
+    (function (Days) {
+        Days[Days["Monday"] = 1] = "Monday";
+        Days[Days["Tuesday"] = 2] = "Tuesday";
+        Days[Days["Wednesday"] = 3] = "Wednesday";
+        Days[Days["Thursday"] = 4] = "Thursday";
+        Days[Days["Friday"] = 5] = "Friday";
+    })(Days || (Days = {}));
+    console.log(Days[dayNum] || 'error');
+}
+dayOfWeek(8);
---
+++ 01-introduction/02-dayOfTheWeek.ts
@@ -0,0 +1,14 @@
+function dayOfWeek(dayNum: number): void {
+
+    enum Days {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+
+    }
+    console.log(Days[dayNum] || 'error');
+}
+
+dayOfWeek(5)
