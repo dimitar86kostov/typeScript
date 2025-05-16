0cc223e72209c77dae7da2eb3444d3f2c2c7a311
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2025-05-16 18:47:52 +0300

    third task is done

---
+++ 01-introduction/03-formatPerson.js
@@ -0,0 +1,4 @@
+function formatP(params) {
+    return "Hello, my name is ".concat(params[0], " and my age is ").concat(params[1]);
+}
+console.log(formatP(['Dimi', 38]));
---
+++ 01-introduction/03-formatPerson.ts
@@ -0,0 +1,6 @@
+function formatP(params:[string, number]) : string {
+    
+    return `Hello, my name is ${params[0]} and my age is ${params[1]}`
+}
+
+console.log(formatP(['Dimi', 38]));
