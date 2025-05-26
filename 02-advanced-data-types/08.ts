40fc7bb9603a82e616e7323d6dcb28bb2f4271fc
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2025-05-26 19:00:14 +0300

    done

---
+++ 02-advanced-data-types/08.ts
@@ -0,0 +1,67 @@
+interface Uuser {
+    username: string, signupDate: Date
+}
+type StatusTask = 'Logged' | 'Started' | 'InProgress' | 'Done';
+
+
+interface Task {
+    status: StatusTask,
+    title: string,
+    daysRequired: number,
+    assignedTo: {
+        username: string,
+        signupDate: Date
+    } | undefined,
+    changeStatus(newStatus: StatusTask): void
+}
+
+function assignTask(user: Uuser, task: Task) {
+
+    if (task.assignedTo == undefined) {
+
+        task.assignedTo = user;
+
+        console.log(`User ${user.username} assigned to task '${task.title}'`);
+
+    }
+
+}
+
+let user = {
+
+username: 'Margaret',
+
+signupDate: new Date(2022, 1, 13),
+
+passwordHash: 'random'
+
+}
+
+let task1 = {
+
+status: <'Logged' | 'Started' | 'InProgress' | 'Done'>'Logged',
+
+title: 'Need assistance',
+
+daysRequired: 1,
+
+assignedTo: undefined,
+
+changeStatus(newStatus: StatusTask) {
+
+this.status = newStatus; }
+
+}
+
+let task2 = {
+
+status: <'Logged' | 'Started' | 'InProgress' | 'Done'>'Done',
+
+title: 'Test',
+
+daysRequired: 12,
+
+assignedTo: undefined, changeStatus(newStatus: StatusTask) { this.status = newStatus; }, moreProps: 300, evenMore: 'wow' } 
+
+assignTask(user, task1); 
+assignTask(user, task2);
